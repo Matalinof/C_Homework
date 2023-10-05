e654f8fe072ef75231d338c1f0dd9e1e9f99e2a3
Надежда Старикова <nadezdastarikova@MacBook-Air-Nadezda.local>
2023-10-05 15:12:41 +0300

    First seminar ? homewor

---
+++ Task00/Program.cs
@@ -0,0 +1,10 @@
+﻿/*
+0. Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя).
+*/
+
+System.Console.Writeline("Введи число: ");
+int number = Convert.ToInt32(Console.Readline())
+
+int result = number*number
+
+System.Console.WriteLine(result);
